2182e9b682a3dd626cdf71b7d0fcbeae00886ae8
Nathan Erwin <nathan.d.erwin@gmail.com>
2019-01-17 20:12:55 -0500

    * added basic classes and config for a simple ping endpoint * added the springfox dependency * added config (commented out) to enable OpenAPI * updated README with OpenAPI URL

---
+++ spring-boot/README.md
@@ -0,0 +1,27 @@
+# Spring Boot Example
+
+Created based on the [Spring Boot](https://spring.io/guides/gs/spring-boot/) documentation.
+
+## Build
+
+```
+mvn clean package
+```
+
+## Run
+
+```
+java -jar target/spring-boot.war
+```
+
+## REST Endpoint
+
+```
+http://localhost:8080/resources/ping
+```
+
+## OpenAPI
+
+```
+http://localhost:8080/v2/api-docs
+```
---
+++ spring-boot/src/main/java/hello/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+// (1)
+//@Configuration
+//@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+//    @Bean
+//    public Docket api() {
+//        return new Docket(SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+
+}
